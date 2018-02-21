a429e2ec874d8c3e6678795b70cd0fd4302575ef
zacci <isaacgtan57@gmail.com>
2018-02-21 13:01:56 +0800

    Print Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        showWelcomeMessage();
+    }
+
+    public static void showWelcomeMessage() {
+        System.out.println("Hello World");
+    }
+}
